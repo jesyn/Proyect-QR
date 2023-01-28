f8d14d6c8ca1295b7aad6e5030f1c5bfe842080b
Jesica Muñoz <jesydynathos@gmail.com>
2023-01-28 18:10:01 -0300

    slider funtion

--- testimonials-slider-react/testimonials/src/components/Button.jsx
+++ testimonials-slider-react/testimonials/src/components/Button.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
-import styles from './Button.module.scss'
+import React from 'react';
+import styles from './Button.module.scss';
 
-function Button() {
+function Button({previousTestimonial, nextTestimonial}) {
+
+
     return (
-        <button> 
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevron-left" width="35" height="35" viewBox="0 0 24 24" stroke-width="1.5" stroke="#9e9e9e" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <button className={styles.button}> 
+            <svg xmlns="http://www.w3.org/2000/svg" 
+            className="icon icon-tabler icon-tabler-chevron-left" 
+            width="28" 
+            height="28" 
+            viewBox="0 0 24 24" 
+            strokeWidth="2.5" 
+            stroke="#9e9e9e" 
+            fill="none" 
+            strokeLinecap="round" 
+            strokeLinejoin="round"
+            onClick={nextTestimonial}>
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                 <polyline points="15 6 9 12 15 18" />
             </svg>
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevron-right" width="35" height="35" viewBox="0 0 24 24" stroke-width="1.5" stroke="#9e9e9e" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            
+            <svg xmlns="http://www.w3.org/2000/svg" 
+            className="icon icon-tabler icon-tabler-chevron-right" 
+            width="28" 
+            height="28" 
+            viewBox="0 0 24 24" 
+            strokeWidth="2.5" 
+            stroke="#9e9e9e" 
+            fill="none" 
+            strokeLinecap="round" 
+            strokeLinejoin="round"
+            onClick={previousTestimonial}>
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                 <polyline points="9 6 15 12 9 18" />
             </svg>
--- testimonials-slider-react/testimonials/src/components/testimonials.jsx
+++ testimonials-slider-react/testimonials/src/components/testimonials.jsx
@@ -1,29 +1,47 @@
-import React from 'react';
+import React, {useState } from 'react';
 import styles from './Testimonials.module.scss'
 import info from "../db/testimonials.json";
 import Button from './Button';
 
 function Testimonials() {
+
+    const [currentTestimonial, setCurrentTestimonial] = useState(0);
+
+    const goToPreviousTestimonial = () => {
+        const isFirstTestimonial = currentTestimonial === 0;
+        const newTestimonial = isFirstTestimonial ? info.length -1 : currentTestimonial -1;
+        setCurrentTestimonial(newTestimonial);
+
+    }
+
+    const goToNextTestimonial = () => {
+        const isLastTestimonial = currentTestimonial === info.length -1;
+        const newTestimonial = isLastTestimonial ? 0 : currentTestimonial + 1;
+        setCurrentTestimonial(newTestimonial);
+        
+        /* console.log("logitud", info.length-1, "actual", currentTestimonial);
+        console.log("es el ultimo", isLastTestimonial);
+        console.log("nuevo", newTestimonial); */
+    }
+
     return (
         <>
-        {info.map((testimonial) => {
-            return  <section key={testimonial.id} className={styles.Testimonials}>
-                        <div className={styles.container}>
-                            <figure className={styles.portrait}>
-                                <img src={testimonial.img} alt={testimonial.alt_img} />
-                                <nav className={styles.nav_button}>
-                                    <Button />
-                                </nav>
-                            </figure>
-                            <p className={styles.bio}> {testimonial.info} </p>
-                            <h3 className={styles.name} > {testimonial.name} </h3>
-                            <h4 className={styles.profession} > {testimonial.profession} </h4>
-                        </div>
-                    </section>
-                    })
-        }
+        {/* {info.map((testimonial) => { */}
+            <section key={info[currentTestimonial].id} className={styles.Testimonials}>
+                <div className={styles.container} >
+                    <figure className={styles.portrait}>
+                        <img src={info[currentTestimonial].img} alt={info[currentTestimonial].alt_img} />
+                        <nav className={styles.nav_button}>
+                            <Button previousTestimonial={goToPreviousTestimonial} nextTestimonial={goToNextTestimonial} />
+                        </nav>
+                    </figure>
+                    <p className={styles.bio}> {info[currentTestimonial].info} </p>
+                    <h3 className={styles.name} > {info[currentTestimonial].name} </h3>
+                    <h4 className={styles.profession} > {info[currentTestimonial].profession} </h4>
+                </div>
+            </section>
         </>
     )
 }
 
-export default Testimonials
+export default Testimonials;
