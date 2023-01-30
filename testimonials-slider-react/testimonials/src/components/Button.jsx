9f861a8d08e179db4cdbc12a319876069e0dbe91
Jesica Muñoz <jesydynathos@gmail.com>
2023-01-30 14:29:22 -0300

    estilos responsive

--- testimonials-slider-react/testimonials/src/components/Button.jsx
+++ testimonials-slider-react/testimonials/src/components/Button.jsx
@@ -6,7 +6,8 @@ function Button({previousTestimonial, nextTestimonial}) {
 
     return (
         <button className={styles.button}> 
-            <svg xmlns="http://www.w3.org/2000/svg" 
+            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="18" className={styles.arrow} onClick={previousTestimonial}><path fill="none" stroke="#8585AC" stroke-width="3" d="M11 1L3 9l8 8"/></svg>
+            {/*  <svg xmlns="http://www.w3.org/2000/svg" 
             className="icon icon-tabler icon-tabler-chevron-left" 
             width="28" 
             height="28" 
@@ -16,12 +17,12 @@ function Button({previousTestimonial, nextTestimonial}) {
             fill="none" 
             strokeLinecap="round" 
             strokeLinejoin="round"
-            onClick={nextTestimonial}>
+            >
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                 <polyline points="15 6 9 12 15 18" />
-            </svg>
-            
-            <svg xmlns="http://www.w3.org/2000/svg" 
+            </svg> */}
+            <svg xmlns="http://www.w3.org/2000/svg" width="13" height="18" className={styles.arrow} onClick={nextTestimonial}><path fill="none" stroke="#8585AC" stroke-width="3" d="M2 1l8 8-8 8"/></svg>
+            {/* <svg xmlns="http://www.w3.org/2000/svg" 
             className="icon icon-tabler icon-tabler-chevron-right" 
             width="28" 
             height="28" 
@@ -31,10 +32,10 @@ function Button({previousTestimonial, nextTestimonial}) {
             fill="none" 
             strokeLinecap="round" 
             strokeLinejoin="round"
-            onClick={previousTestimonial}>
+            >
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                 <polyline points="9 6 15 12 9 18" />
-            </svg>
+            </svg> */}
         </button>
     )
 }
